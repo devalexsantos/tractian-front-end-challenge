e7b22a579d54f384ccfd2d5fe6d9b35121977572
devalexsantos <devalexsantos@gmail.com>
2024-12-04 15:36:23 +0000

    chore: create a location and asset types

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export interface Location {
+  id: string;
+  name: string;
+  parentId?: string | null;
+  type: 'location';
+  children: Array<Location | Asset>;
+}
+
+export interface Asset {
+  id: string;
+  name: string;
+  locationId?: string | null;
+  parentId?: string | null;
+  sensorType?: string | null;
+  sensorId?: string | null;
+  status?: 'operating' | 'alert' | 'critical' | null;
+  gatewayId?: string | null;
+  type: 'asset' | 'component';
+  children: Asset[];
+}
+
+
